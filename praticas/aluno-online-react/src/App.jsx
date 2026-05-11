41a1730d3c1381c26a05355d48e806e69745c787
jhonata295 <161323307+jhonata295@users.noreply.github.com>
2026-05-11 09:33:29 -0300

    Revert "Feature/pratica06"

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,23 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
-
-import Layout from './layouts/Layout'
-
-import Dashboard from './pages/Dashboard'
-import Faltas from './pages/Faltas'
-import Notas from './pages/Notas'
-import Boletos from './pages/Boletos'
+import Login from './pages/Login';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="faltas" element={<Faltas />} />
-        <Route path="notas" element={<Notas />} />
-        <Route path="boletos" element={<Boletos />} />
-      </Route>
-    </Routes>
-  )
+  return <Login />;
 }
 
-export default App
+export default App;
